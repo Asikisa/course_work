eca0083d701dbee07fa289aa849ddd0a49fe349b
Asikisa <akruckova01@gmail.com>
2021-05-08 16:01:56 +0300

    no mdp more numbers working properly

--- noMDP_expected_income.py
+++ noMDP_expected_income.py
@@ -1,14 +1,18 @@
 import numpy as np
 
 # default matrix
-R = np.array([[9, 3, 1],
-              [3, -7, 1],
-              [3, -7, 1]])
+# R = np.array([[9, 3, 1],
+#               [3, -7, 1],
+#               [3, -7, 1]])
+R = np.array([[9, 3],
+              [3, -7]])
 
 # probability matrix
-P = np.array([[0.4, 0.4, 0.2],
-              [0.4, 0.5, 0.1],
-              [0.5, 0.3, 0.2]])
+# P = np.array([[0.4, 0.4, 0.2],
+#               [0.4, 0.5, 0.1],
+#               [0.5, 0.3, 0.2]])
+P = np.array([[0.5, 0.5],
+              [0.4, 0.6]])
 
 MAX_IT = 10
 STATE_NUMBER = len(R)
@@ -28,8 +32,9 @@ for i in range(MAX_IT):
             Q[j] = summ
     else:
         for j in range(STATE_NUMBER):
+            summ = 0  # ????????
             for k in range(STATE_NUMBER):
-                a = P[j][k]*result_matrix[j-1][k]
+                a = P[j][k]*result_matrix[i-1][k]
                 summ += a
             result_matrix[i][j] = summ + Q[j]
 
