5a0373db02eb00f6f4abe5d24451ef5d1e3bca23
Asikisa <akruckova01@gmail.com>
2021-05-08 14:06:33 +0300

    multiple dims added

--- noMDP_expected_income.py
+++ noMDP_expected_income.py
@@ -1,32 +1,37 @@
 import numpy as np
 
 # default matrix
-R = np.array([[9, 3],
-              [3, -7]])
+R = np.array([[9, 3, 1],
+              [3, -7, 1],
+              [3, -7, 1]])
 
 # probability matrix
-P = np.array([[0.5, 0.5],
-              [0.4, 0.6]])
-
-# coefficient matrix
-Q = [0, 0]
+P = np.array([[0.4, 0.4, 0.2],
+              [0.4, 0.5, 0.1],
+              [0.5, 0.3, 0.2]])
 
 MAX_IT = 10
 STATE_NUMBER = len(R)
+
+# coefficient matrix
+Q = np.zeros(STATE_NUMBER)
 
 result_matrix = np.zeros((MAX_IT, STATE_NUMBER))
 for i in range(MAX_IT):
     if i == 0:
         for j in range(STATE_NUMBER):
-            a = P[j][0]*R[j][0]
-            b = P[j][1]*R[j][1]
-            result_matrix[i][j] = a + b
-            Q[j] = a + b
+            summ = 0
+            for k in range(STATE_NUMBER):
+                a = P[j][k]*R[j][k]
+                summ += a
+            result_matrix[i][j] = summ
+            Q[j] = summ
     else:
         for j in range(STATE_NUMBER):
-            a = P[j][0]*result_matrix[i-1][0]
-            b = P[j][1]*result_matrix[i-1][1]
-            result_matrix[i][j] = a + b + Q[j]
+            for k in range(STATE_NUMBER):
+                a = P[j][k]*result_matrix[j-1][k]
+                summ += a
+            result_matrix[i][j] = summ + Q[j]
 
 print(result_matrix)
 
